0d35bd49d325d2bee85c95c53024cf7cb6717b8b
WangRui <maxieewong@gmail.com>
2016-10-14 23:40:33 +0800

    pageTest.

---
+++ test/pageTest.py
@@ -0,0 +1,15 @@
+from db.models import *
+
+fragment1 = Fragment.create(content="比起上学, 上了班以后, 时间明显减少了.")
+
+fragment2 = Fragment.create(content="但是在工作中能力提升地比较快.")
+
+fragment3 = Fragment.create(content="想写的程序更多了, 卒.")
+
+fragmentList = [fragment1.id, fragment2.id, fragment3.id]
+
+page = Page.create(
+    title="工作感悟",
+    fragments=str(fragmentList))
+
+
